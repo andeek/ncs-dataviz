8cae667fb6f278df2791291ab2f0912e9f45351b
Andee Kaplan <ajkaplan@iastate.edu>
2015-06-18 15:59:08 -0500

    margins

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 var margin = {top: 95, right: 10, bottom: 25, left: 75},
-    width = 350 - margin.left - margin.right,
+    width = 325 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 var y = d3.scale.ordinal()
